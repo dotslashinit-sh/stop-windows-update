b7fcbcfb168f2f86f0b3356f096914d493752674
Abin R <68489194+dotslashinit-sh@users.noreply.github.com>
2021-11-11 10:20:10 +0530

    Add files via upload

---
+++ StopUpdate.bat
@@ -0,0 +1,15 @@
+@ECHO OFF
+REM Windows Update stop batch script
+
+REM This script will force stop windows update but it may start again automatically after a few minutes.
+REM You can run it again to stop it.
+
+echo Stopping windows update...
+sc stop wuauserv
+sc stop UsoSvc
+
+REM Delivery optimization
+sc stop DoSvc
+
+echo Stopped windows update. It might automatically start after a few minutes, and you can run this script again at that time to stop it.
+pause
